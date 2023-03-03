2ccb0ad7153561de0ff4c3e8850f6ed9d06f3a99
phanhduchuy <phanhoangduchuyy@gmail.com>
2023-03-03 19:05:00 +0700

    xong authenlogin/logout voi alert

--- MyOToVer1.2/Controllers/HomeController.cs
+++ MyOToVer1.2/Controllers/HomeController.cs
@@ -51,15 +51,20 @@ namespace MyOToVer1._2.Controllers
         [HttpPost]
         public IActionResult Register(Customer obj)
         {
-            bool checkContact = _db.Customers.Any(c => c.Contact.Equals(obj.Contact));
-            if (checkContact)
+            if (ModelState.IsValid)
             {
-                return Content("Contact is already exist");
+                bool checkContact = _db.Customers.Any(c => c.Contact.Equals(obj.Contact));
+                if (checkContact)
+                {
+                    ModelState.AddModelError(nameof(Customer.Contact), "Số điện thoại đã tồn tại");
+                    return View();
+                }
+                obj.Password = EncryptPassword(obj.Password);
+                _db.Customers.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index", "Home");
             }
-            obj.Password = EncryptPassword(obj.Password);
-            _db.Customers.Add(obj);
-            _db.SaveChanges();
-            return RedirectToAction("Login", "Home");
+            return View(obj);
         }
 
         //Get
@@ -72,28 +77,39 @@ namespace MyOToVer1._2.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Login(string contact, string password)
         {
-
-            ApplicationDBContext dBContext = _db;
-            var data = dBContext.Customers.Where(e => e.Contact.Equals(contact)).SingleOrDefault();
-
-            bool isValidContact = dBContext.Customers.Any(dbcontext => dbcontext.Contact.Equals(contact));
-            if (isValidContact)
+            if (ModelState.IsValid)
             {
-                bool isValidPassword = dBContext.Customers.Any(dbcontext => dbcontext.Contact.Equals(contact) && DecryptPassword(data.Password) == password);
-                if (isValidPassword)
+                ApplicationDBContext dBContext = _db;
+                var data = dBContext.Customers.Where(e => e.Contact.Equals(contact)).SingleOrDefault();
+
+                bool isValidContact = dBContext.Customers.Any(dbcontext => dbcontext.Contact.Equals(contact));
+                if (isValidContact)
                 {
-                    var claims = new List<Claim>
+                    bool isValidPassword = dBContext.Customers.Any(dbcontext => dbcontext.Contact.Equals(contact) && DecryptPassword(data.Password) == password);
+                    if (isValidPassword)
+                    {
+                        var claims = new List<Claim>
                 {
                     new Claim(ClaimTypes.SerialNumber, contact)
                 };
-                    var identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
-                    var principal = new ClaimsPrincipal(identity);
-                    var props = new AuthenticationProperties();
-                    HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal, props).Wait();
-                    return RedirectToAction("MainPage", "Customer");
+                        var identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                        var principal = new ClaimsPrincipal(identity);
+                        var props = new AuthenticationProperties();
+                        HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal, props).Wait();
+                        return RedirectToAction("MainPage", "Customer");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError(nameof(Customer.Password), "Mật khẩu không đúng");
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError(nameof(Customer.Contact), "Số điện thoại không tồn tại");
                 }
             }
             return View();
+            
         }
         [HttpPost]
         public async Task<IActionResult> Logout()
